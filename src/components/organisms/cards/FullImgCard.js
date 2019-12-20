5da03a96cb2e8c963706d6d7558e57e9b1b97f26
Rio Sanka <threehouse.second@gmail.com>
2019-12-20 10:16:13 +0900

    cardコンポーネント organisms

---
+++ src/components/organisms/cards/FullImgCard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+
+import CardText from '../../atoms/card/CardText';
+import CardTitle from '../../molecules/card/CardTitle';
+
+export default class FullImgCard extends React.Component {
+  render() {
+    const { cardSubTitle, cardMainTitle, cardText } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Image 
+          style={styles.image}
+          source={require('../../../../assets/sample_images/cardImg_lg_sample.png')}
+        />
+
+        <View style={styles.cardTitleContainer}>
+          <CardTitle 
+            cardSubTitle={cardSubTitle} 
+            cardMainTitle={cardMainTitle} 
+          />
+        </View>
+
+        <View  style={styles.cardTextContainer}>
+          <CardText cardText={cardText}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    overflow: 'hidden',
+    borderRadius: 15,
+    height: 400, 
+    marginBottom: 30,
+
+    borderColor:'#f7f6fb',
+    borderWidth:2,
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+  },
+  cardTitleContainer: {
+    position: 'absolute',
+    width: 300,
+    top: 17,
+    left: 13,
+  },
+  cardTextContainer: {
+    position: 'absolute',
+    width: 300,
+    bottom: 17,
+    left: 20,
+  },
+});
